e62bed2cd61ffeda8caaa534a9f0ad5c49563a1c
harjeeyborlah <harjeeyborlah@gmail.com>
2023-01-09 19:25:04 +0100

    First day of 2021/2022 2nd term 2

--- public/index.js
+++ public/index.js
@@ -20,3 +20,21 @@ burger.addEventListener('click' , ()=>{
 })
 
 // // //when burger is clicked, look at menu if it contains hidden class, remove to show the menu, if not when clicked again add hidden
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Hide";
+      moreText.style.display = "inline";
+    }
+  } 
+
+  // for the read more and read less portion
