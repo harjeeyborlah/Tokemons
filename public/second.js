818073efc771a3b791afdc26994c342dd4abd52d
harjeeyborlah <harjeeyborlah@gmail.com>
2023-02-07 12:44:09 +0100

    07/02/2023 MORNING

--- public/second.js
+++ public/second.js
@@ -10,4 +10,18 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1.1,
     centeredSlides:true,
     spaceBetween:2,
+
+
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+    }
   });
